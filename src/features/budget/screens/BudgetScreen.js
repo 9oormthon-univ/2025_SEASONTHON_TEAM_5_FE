12e1210c954ceff253d31dd64e37b5196f3f5f28
ghddbsdl041 <hong.yy041@gmail.com>
2025-09-07 03:51:10 +0900

    feat: 예산관리 페이지 수정

--- src/features/budget/screens/BudgetScreen.js
+++ src/features/budget/screens/BudgetScreen.js
@@ -4,7 +4,6 @@ import { ScrollView, View, StyleSheet } from "react-native";
 import BudgetHeader from "../components/BudgetHeader";
 import BudgetSetup from "../components/BudgetSetup";
 import FoodRegister from "../components/FoodRegister";
-import GotoRecipe from "../components/GotoRecipe";
 
 export default function BudgetScreen() {
   return (
@@ -13,7 +12,6 @@ export default function BudgetScreen() {
         <BudgetHeader />
         <BudgetSetup />
         <FoodRegister />
-        <GotoRecipe />
         <View style={{ height: 24 }} />
       </ScrollView>
     </SafeAreaView>
